43a49ff8df5ceef7a99ab1ef59747ab0b45dbcbd
Dean Bourgeois <deanbourgeois@OFrenchy.local>
2019-02-15 07:07:20 -0600

    Initial push w/Node class

---
+++ Node.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearch
+{
+    class Node
+    {
+        // Member variables
+        int value;
+        Node baseNode;
+        Node leftNode;
+        Node rightNode;
+
+        // Constructor
+        public Node(int value, Node baseNode, Node leftNode = null, Node rightNode = null)
+        {
+            this.value = value;
+            this.baseNode = baseNode;
+            if (leftNode != null) this.leftNode = leftNode;
+            if (rightNode != null) this.rightNode = rightNode;
+        }
+        // Member methods - … Can Do …
+
+        public int Value { get; }
+
+        public Node Find (int searchValue)
+        {
+            Node returnNode;
+
+
+
+            // always start the search at the base;  if this node's base is null, 
+            // start here, because the only node with null for the base is the first one
+            if (baseNode == null)
+            {
+                returnNode = Find(searchValue);
+            }
+            else 
+                returnNode = baseNode.Find(searchValue);
+
+            return returnNode;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Diagnostics;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
